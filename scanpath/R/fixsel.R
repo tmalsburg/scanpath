4cb5f3e3720cdc73aaaeea08fd8b201ce7e5d9b5
Titus von der Malsburg <malsburg@posteo.de>
2014-02-07 18:24:47 +0000

    Added support for nth to find.fixation.
    Also added inline documentation.

--- scanpath/R/fixsel.R
+++ scanpath/R/fixsel.R
@@ -2,7 +2,7 @@ pcre <- function(pattern, text, subpattern)
 {
 
   stopifnot(as.integer(subpattern)==subpattern)
-  .Call("_gregexpr",pattern,text,subpattern)
+  .Call("_gregexpr", pattern, text, subpattern)
 
 }
 
@@ -18,9 +18,13 @@ pcre <- function(pattern, text, subpattern)
 #' @param res.type a function for converting the result vector to a
 #' desired data type.
 #' @param na.value a replacement value for NAs.
+#' @param ... extra arguments that are passed to \code{res.type}.
 #' @return a version of \code{x} in which all original values are
 #' substituted by the replacements specified in \code{r}.
-#' @note This function can be used to replace region identifiers 
+#' @note This function can be used to replace arbitrary region
+#' identifiers in fixation data by single letters.  This format is
+#' needed by the functions \code{\link{find.fixation}} and
+#' \code{\link{match.scanpath}}.
 #' @export
 #' @examples
 #' replace.all(1:10, letters[1:10])
@@ -43,86 +47,76 @@ replace.all <- function(x, r, res.type=as.character, na.value=NA, ...) {
 
 }
 
-# Get that package from my webpage:
-# library(scanpath)
-# plot.scanpaths(dur~wn|id, d)
-
-#
-# Finding specific events in the eye movement record:
-#
-# TODO: nth!=1 case
-# TODO: groups
-
-# Implementation: pseudo code for the case without groups: (groups are just
-# ignored)
-
-# * syms is the sequence of word identifiers in the order in which they were
-#   fixated.
-# * trial indicates to which trial each fixation belongs.
-# * expr is the regular expression.
-# * nth indicates which match should be returned.  If nth is larger than the
-#   number of matches in a trial, nothing is returned for that trial.
-# * group indicates which group we're interested in.  If group==1, pointers to
-#   the beginning of the first group are returned.  (Throw an error message if
-#   a group is requested that doesn't exist in the expression.)  If
-#   is.null(group), ignore groups and return pointers to the beginning of the
-#   global match.
-find.fixation <- function(syms, trial, expr, nth=1, subpattern=0) {
+#' This function uses regular expressions to identify elements in a
+#' vector of letters where a specific pattern begins.  Among other
+#' things, this can be used to identify certain eye movement patterns
+#' in fixation data.
+#'
+#' @title Mark the beginning of specified pattern in a letter sequence
+#' @param l a vector of single characters.
+#' @param groups a grouping variable.  The search for matching
+#' patterns in performed in each group separately.  Matches crossing
+#' group boundaries are not considered.
+#' @param expr a regular expression describing the pattern.
+#' @param nth specifies which match should be returned if there were
+#' several within a group.  The default is to return all matches.
+#' @param subpattern the subpattern of interest.  If zero, the
+#' beginning of the full pattern will be marked.  If n>0, the
+#' beginning of the n-th subpattern will be marked.  See examples.
+#' @return A vector giving the indices in l where the matches
+#' begin.  If subpattern > 0, then the indices will point to the
+#' beginning of the subpatterns.
+#' @export
+#' @examples
+#' data(eyemovements)
+#' words <- eyemovements$word
+#' l <- replace.all(words, letters[1:length(unique(words))])
+#' trial <- eyemovements$trial
+#' # Find fixations on word 6 (which is represented by letter "f"):
+#' idx <- find.fixation(l, trial, "f")
+#' words[idx]
+#' # Find fixations on word 6 but only the second match within
+#' # a group (i.e., trial):
+#' idx <- find.fixation(l, trial, "f", nth=2)
+#' words[idx]
+#' # Find fixations on word 6 that are followed by fixations on word 7 ("g"):
+#' idx <- find.fixation(l, trial, "fg")
+#' words[idx]
+#' # Find those fixations on word 6 that followed fixations on word 5 ("e"):
+#' idx <- find.fixation(l, trial, "e(f)", subpattern=1)
+#' words[idx]
+#' # Find the beginning of a second sweep over the sentence:
+#' idx <- find.fixation(l, trial, "a[b-h]+(a+[b-g]+)", subpattern=1)
+#' words[idx]
+find.fixation <- function(l, groups, expr, nth=NA, subpattern=0) {
   
-  if (nth!=1)
-    stop("use gregepxr")
+  stopifnot(all(nchar(l)==1))
+  stopifnot(is.na(nth)||floor(nth)==nth)
+  stopifnot(is.na(nth)||nth>0)
 
-  s <- sapply(split(syms, trial),
-              function(s) do.call(paste, c(as.list(s), sep="")))
+  s <- tapply(l, groups, paste, collapse="")
 
-  hits <- unlist(pcre(expr, s, subpattern))
-  offsets <- c(0, cumsum(nchar(s)))[1:length(hits)]
-
-  offsets <- offsets[hits!=-1]
-  hits <- hits[hits!=-1]
+  hits <- pcre(expr, s, subpattern)
+  offsets <- c(0, cumsum(nchar(s)[-length(s)]))
+  names(offsets) <- unique(groups)
+  # Remove no-match entries:
+  hits <- lapply(hits, function(h) if (h[[1]]!=-1) h)
+  if (!is.na(nth))
+    hits <- lapply(hits, function(h) if (length(h)>=nth) h[[nth]])
+  # Count matches per string:
+  nm <- sapply(hits, length)
+  # Repeat offsets of string with several matches:
+  offsets <- rep(offsets, nm)
+  
+  hits <- unlist(hits)
 
   hits + offsets
 
 }
 
-# # Fixations on word F:
-# find.fix(d$wn, d$id, "F")
-# d[find.fix(d$wn, d$id, "F"),]
-# 
-# # Fixations on word F followed by a fixation on word E:
-# find.fix(d$wn, d$id, "FE")
-# 
-# # Fixations on word F preceded by a fixation on word G:
-# # (We want a pointer for E not G.)
-# find.fix(d$wn, d$id, "(G)E")
-# 
-# # Beginngin of a second pass through the sentence:
-# find.fix(d$wn, d$id, "([A-G]+)A[A-G]+")
+match.scanpath <- function(l, groups, expr, subpattern=0) {
 
-#
-# Extract sub-scanpaths:
-#
-# TODO: nth!=1 cases
-# TODO: groups
-
-# * syms is the sequence of word identifiers in the order in which they were
-#   fixated.
-# * trial indicates to which trial each fixation belongs.
-# * expr is the regular expression.
-# * nth indicates which match should be returned.  If nth is larger than the
-#   number of matches in a trial, nothing is returned for that trial.
-# * group indicates which group we're interested in.  If group==1, the scanpath
-#   corresponding to the first group is returned.  (Throw an error message if
-#   a group is requested that doesn't exist in the expression.)  If
-#   is.null(group), ignore groups and return the scanpath corresponding to the
-#   global match.
-match.scanpath <- function(syms, trial, expr, nth=1, subpattern=0) {
-
-  if (nth!=1)
-    stop("use gregepxr")
-
-  s <- sapply(split(syms, trial),
-              function(s) do.call(paste, c(as.list(s), sep="")))
+  s <- tapply(l, groups, paste, collapse="")
 
   hitsg <- pcre(expr, s, subpattern)
   hits <- sapply(hitsg, function(x) x[[1]])
